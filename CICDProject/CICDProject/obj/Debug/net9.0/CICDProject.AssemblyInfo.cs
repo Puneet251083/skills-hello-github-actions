bc7e82a8a748e787262c3f6fd0da286ebafd971c
Puneet251083 <55555983+Puneet251083@users.noreply.github.com>
2026-07-29 19:02:33 +0530

    updated project

--- CICDProject/CICDProject/obj/Debug/net9.0/CICDProject.AssemblyInfo.cs
+++ CICDProject/CICDProject/obj/Debug/net9.0/CICDProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CICDProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1816bab78b91c39488f0f73f8848c0a00197d965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd0f5c8b3ff02e36d2cc189664b01e75fe05321")]
 [assembly: System.Reflection.AssemblyProductAttribute("CICDProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CICDProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
